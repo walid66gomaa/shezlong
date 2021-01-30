8f70152b1a3ed82e56e9a59d00a3ad51974ef3e8
walid elbehery <iam66walid@gmail.com>
2021-01-30 14:24:37 +0200

    add sucsses-steps

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,6 +1,21 @@
 @extends('frontend.layouts.app')
 
 @section('title', config('app.name') . ' | ' . __('home'))
+<section   class=" sucsses-steps">
+    <div   class="container">
+        <div   class="row text-white">
+            <div   class="col-md-12 text-center steps-title">3 خطوات نحو حياة افضل </div>
+
+            <div   class="col-md-2 margin-md-16  sucsses-step active">اختار المعالج</div>
+            <div   class="col-md-1 seprator"></div>
+
+            <div   class="col-md-2  sucsses-step">حدد التاريخ والوقت</div>
+            <div   class="col-md-1 seprator"></div>
+            
+            <div   class="col-md-2  sucsses-step">أكمل عملية الدفع</div>
+        </div>
+    </div>
+</section>
 
 @section('content')
 @endsection
