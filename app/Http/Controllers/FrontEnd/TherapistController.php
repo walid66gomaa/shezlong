9d81ab3ed9cafbd57434552f345d5d3a3879bca8
walid elbehery <iam66walid@gmail.com>
2021-02-01 22:56:16 +0200

    refactor

--- app/Http/Controllers/FrontEnd/TherapistController.php
+++ app/Http/Controllers/FrontEnd/TherapistController.php
@@ -15,41 +15,43 @@ class TherapistController extends Controller
 	public function search(Request $filters)
 	{
 
-		
+		$validated = $filters->validate([
+			'therapistName' => 'string',
+			'orderBy' => 'string',
+			'specialtyName' => 'string',
+
+		]);
+
 		$therapists = (new Therapist)->newQuery();
 
-		if ($filters->has('therapistName') && !empty(  $filters->therapistName)) {
+		if ($filters->has('therapistName') && !empty($filters->therapistName)) {
 			//full text search
 			$therapists->whereRaw(
 				"MATCH(name) AGAINST(?)",
 				array($filters->therapistName)
 			);
 		}
-		
-		if ($filters->has('specialtyName') &&  !empty(  $filters->specialtyName)) {
+
+		if ($filters->has('specialtyName') &&  !empty($filters->specialtyName)) {
 
 			$specialty = Specialty::select('id')->where('specialtyName', $filters->specialtyName)->first();
 			if ($specialty) {
 				$therapists->join('specialty_therapist', 'therapists.id', '=', 'specialty_therapist.therapist_id')
 					->where('specialty_therapist.specialty_id', $specialty->id);
 			}
-	
 		}
-	
-		if($filters->has('orderBy') && !empty(  $filters->orderBy) )
-		{
+
+		if ($filters->has('orderBy') && !empty($filters->orderBy)) {
 			// price_ASC , price_DESC , rate_DESC
-		$sortArr= explode('_',$filters->orderBy);	
-		$therapists->orderBy($sortArr[0],$sortArr[1]);
-
-
+			$sortArr = explode('_', $filters->orderBy);
+			$therapists->orderBy($sortArr[0], $sortArr[1]);
 		}
 
-		$therapists=$therapists->get();
-		$specialties= Specialty::all();
-	
+		$therapists = $therapists->get();
+		$specialties = Specialty::all();
 
-	
-			return view('frontend.index', compact('therapists' ,'specialties'));
+
+
+		return view('frontend.index', compact('therapists', 'specialties'));
 	}
 }
